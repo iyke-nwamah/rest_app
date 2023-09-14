2169401700f2301d32a72e7f681906e2c4bbb81b
oishbdh-2 <ikechukwuarinze614@gmail.com>
2023-09-14 23:42:52 +0100

    rest

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Notification = () => {
   return (
-    <div className='h-12 bg-black-500 text-white px-4 flex items-left justify-center text-left text-sm md:text-base cursor-pointer'> Opening Hour: 9;00am - 10:00pm
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-start text-center text-sm md:text-base cursor-pointer'>
+      Opening Hour: 9:00am - 10:00pm
     </div>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
